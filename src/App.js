4abc21da4bb51bcadedf2d02dec92689683c0afc
caroline-gao <carolinegao@utexas.edu>
2025-04-01 14:59:35 -0500

    change to hash routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Onboarding from "./pages/OnboardingPage";
 import Indoor from "./pages/IndoorPage";
 import Outdoor from "./pages/OutdoorPage";
 import OutdoorEditPage from "./pages/OutdoorEditPage";
-import CalendarPage from "./pages/CalendarPage"
+import CalendarPage from "./pages/CalendarPage";
 import IndoorEdit from "./pages/IndoorEditPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Onboarding />} />
-        <Route path={`${process.env.PUBLIC_URL}/my-meadow-a5/indoor`} element={<Indoor />} />
-        <Route path={`${process.env.PUBLIC_URL}/my-meadow-a5/outdoor`} element={<Outdoor />} />
-        <Route path={`${process.env.PUBLIC_URL}/my-meadow-a5/outdoor/edit`}element={<OutdoorEditPage />} />
-        <Route path={`${process.env.PUBLIC_URL}/my-meadow-a5/calendar`} element={<CalendarPage />} />
-        <Route path={`${process.env.PUBLIC_URL}/my-meadow-a5/indoor/edit`} element={<IndoorEdit />} />
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/indoor" element={<Indoor />} />
+        <Route path="/outdoor" element={<Outdoor />} />
+        <Route path="/outdoor/edit" element={<OutdoorEditPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/indoor/edit" element={<IndoorEdit />} />
       </Routes>
     </Router>
   );
